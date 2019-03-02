20610c9e7dbc9256948c3c33e039c1eace74d3dc
Shubham Singh <201652026@iiitvadodara.ac.in>
2019-03-02 19:56:24 +0530

    Navbar

--- src/components/dashbord/Navbar.jsx
+++ src/components/dashbord/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import logo from "../../static/image/logo.png";
+import logo from "../../static/image/logo_cric.png";
 import "../../static/css/Navbar.css";
 import { Link, NavLink } from "react-router-dom";
 
@@ -11,72 +11,31 @@ class Navbar extends Component {
   render() {
     return (
       <div>
-        <nav className="navbar navbar-expand-md navbar-dark fixed-top">
-          <Link to="/" className="navbar-brand">
-            <img src={logo} alt="cricLogo" className="logoImg" />
-          </Link>
-
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#collapsibleNavbar"
-          >
-            <span className="navbar-toggler-icon" />
-          </button>
-          <div className="collapse navbar-collapse" id="collapsibleNavbar">
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <NavLink to="/" className="nav-link" id="nav_item_color">
-                  Home
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink to="/teams" className="nav-link" id="nav_item_color">
-                  Teams
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink
-                  to="/fixtures"
-                  className="nav-link"
-                  id="nav_item_color"
-                >
-                  Fixtures
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink to="/results" className="nav-link" id="nav_item_color">
-                  Results
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink to="/status" className="nav-link" id="nav_item_color">
-                  Stats
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink
-                  to="/register"
-                  className="nav-link"
-                  id="nav_item_color"
-                >
-                  Register
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink to="/images" className="nav-link" id="nav_item_color">
-                  Images
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink to="/about" className="nav-link" id="nav_item_color">
-                  About us
-                </NavLink>
-              </li>
-            </ul>
+        <div className="nav">
+          <div className="nav--left">
+            <div className="cl">
+              <h1>
+                CRICKET <span>LEAGUE</span>
+              </h1>
+            </div>
+            <div className="navLeft--underline" />
           </div>
-        </nav>
+          <div className="nav--logo">
+            <img className="logo" src={logo} alt="Cric logo" />
+          </div>
+
+          <div className="nav--right">
+            <ul className="navlink">
+              <li>
+                <span>HOME</span>
+              </li>
+              <li>FIXTURES</li>
+              <li>REGISTER</li>
+              <li>CONTACT</li>
+            </ul>
+            <div className="navRight--underline" />
+          </div>
+        </div>
       </div>
     );
   }
