340507bcbfccd4889239b3b69371e82e92bb2982
Shubham Singh <201652026@iiitvadodara.ac.in>
2019-03-17 11:51:20 +0530

    u

--- build/precache-manifest.4932c20bda98430de30d6e50bd45bb2f.js
+++ build/precache-manifest.4932c20bda98430de30d6e50bd45bb2f.js
@@ -52,12 +52,12 @@ self.__precacheManifest = [
     "url": "/IIITVCricLeague/static/media/fontawesome-webfont.fee66e71.woff"
   },
   {
-    "revision": "b06871f281fee6b241d60582ae9369b9",
-    "url": "/IIITVCricLeague/static/media/fontawesome-webfont.b06871f2.ttf"
-  },
-  {
     "revision": "674f50d287a8c48dc19ba404d20fe713",
     "url": "/IIITVCricLeague/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/IIITVCricLeague/static/media/fontawesome-webfont.b06871f2.ttf"
   },
   {
     "revision": "912ec66d7572ff821749319396470bde",
