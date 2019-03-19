c86a2d8ad69ac7a2adc61bcc6700af98586a5aeb
Shubham Singh <201652026@iiitvadodara.ac.in>
2019-03-19 20:28:44 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import Navbar from "./components/dashbord/Navbar";
 import { Route } from "react-router-dom";
 import Home from "./components/dashbord/Home";
 import "./App.css";
@@ -8,23 +7,18 @@ import signUpOwner from "./components/auth/signUpOwner";
 import signUpPlayer from "./components/auth/signUpPlayer";
 import { BrowserRouter } from "react-router-dom";
 import Stats from "./components/stats/stats";
-import Footer from "./components/dashbord/footer";
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <div>
-          {/* <Navbar /> */}
-          <React.Fragment>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/register" component={ForRegistration} />
-            <Route exact path="/register/owner" component={signUpOwner} />
-            <Route exact path="/register/player" component={signUpPlayer} />
-            <Route exact path="/status" component={Stats} />
-          </React.Fragment>
-          {/* <Footer /> */}
-        </div>
+        <React.Fragment>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/register" component={ForRegistration} />
+          <Route exact path="/register/owner" component={signUpOwner} />
+          <Route exact path="/register/player" component={signUpPlayer} />
+          <Route exact path="/status" component={Stats} />
+        </React.Fragment>
       </BrowserRouter>
     );
   }
